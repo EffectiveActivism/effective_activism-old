34ddb160dd953b530390ba2ad6342d3676452715
Roensby <peter@roensby.dk>
2016-04-19 20:46:28 +0200

    Added settings page

---
+++ modules/ea_locations/src/Form/LocationsSettingsForm.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ea_locations\Form\LocationsSettingsForm.
+ */
+
+namespace Drupal\ea_locations\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class LocationsSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'ea_location_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEditableConfigNames() {
+    return ['ea_locations.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('ea_locations.settings');
+    $form['key'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Google Maps API key'),
+      '#description' => $this->t('An API key is needed to retrieve data from the Google Maps API. To get a key, create a Google account and go to @link', ['@link' => 'https://code.google.com/apis/console']),
+      '#required' => TRUE,
+      '#default_value' => $config->get('key'),
+    );
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('ea_locations.settings')
+      ->set('key', $form_state['values']['key'])
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
