e3f1d2a249e58d1da86b24e4340fb42c0b0ee1c6
Roensby <peter@roensby.dk>
2016-04-23 12:25:32 +0200

    Initial commit

---
+++ modules/ea_tasks/src/Form/TaskDeleteForm.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ea_tasks\Form\TaskDeleteForm.
+ */
+
+namespace Drupal\ea_tasks\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Task entities.
+ *
+ * @ingroup ea_tasks
+ */
+class TaskDeleteForm extends ContentEntityDeleteForm {
+
+}
