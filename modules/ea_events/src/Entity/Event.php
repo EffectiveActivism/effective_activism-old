46dfc92b065c4a9c2497d0ad688394ae604c26d7
Roensby <peter@roensby.dk>
2016-04-23 14:13:55 +0200

    Added task field

--- modules/ea_events/src/Entity/Event.php
+++ modules/ea_events/src/Entity/Event.php
@@ -226,9 +226,10 @@ class Event extends ContentEntityBase implements EventInterface {
       ))
       ->setDisplayConfigurable('form', TRUE)
       ->setDisplayConfigurable('view', TRUE);
-    $fields['participants'] = BaseFieldDefinition::create('entity_reference')
-      ->setLabel(t('Participants'))
-      ->setSetting('target_type', 'person')
+    $fields['tasks'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Tasks'))
+      ->setDescription(t('The tasks to do before, under and after the event.'))
+      ->setSetting('target_type', 'task')
       ->setSetting('handler', 'default')
       ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)
       ->setDisplayOptions('view', array(
@@ -238,7 +239,7 @@ class Event extends ContentEntityBase implements EventInterface {
         'type' => 'inline_entity_form_complex',
         'settings' => array(
           'allow_new' => TRUE,
-          'allow_existing' => TRUE,
+          'allow_existing' => FALSE,
         ),
       ))
       ->setDisplayConfigurable('form', TRUE)
