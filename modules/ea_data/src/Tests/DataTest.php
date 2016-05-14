c639c494ac538a26c5f0563053572c590ae7551b
Roensby <peter@roensby.dk>
2016-05-14 13:21:40 +0200

    Refactored tests

--- modules/ea_data/src/Tests/DataTest.php
+++ modules/ea_data/src/Tests/DataTest.php
@@ -19,31 +19,42 @@ class DataTest extends WebTestBase {
 
   public static $modules = array('ea_data', 'field_ui');
 
-  private $user;
+  private $statistician;
+  private $organizer;
 
   /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
-    $this->user = $this->drupalCreateUser(array(
-      'add data entities',
+    $this->statistician = $this->drupalCreateUser(array(
       'administer data entities',
+      'administer data fields'
+    ));
+    $this->organizer = $this->drupalCreateUser(array(
+      'add data entities',
       'delete data entities',
       'edit data entities',
       'view data entities',
-      'administer data fields'
     ));
   }
 
   /**
-   * Test the data entity.
-   * 
-   * Create a data type and add an integer field to it.
-   * Then create a data entity using the data type. 
+   * Create a data entity.
    */
   public function testDataEntity() {
-    $this->drupalLogin($this->user);
+    $this->createDataType();
+    $this->createDataEntity();
+  }
+
+  /**
+   * Create a data type.
+   * 
+   * Creates a data type called data_type_test and adds a numeric
+   * field field_integer_input to it.
+   */
+  private function createDataType() {
+    $this->drupalLogin($this->statistician);
     $this->drupalGet('effectiveactivism/data_type');
     $this->assertResponse(200);
     $this->drupalGet('effectiveactivism/data_type/add');
@@ -78,12 +89,23 @@ class DataTest extends WebTestBase {
     ), t('Save settings'));
     $this->assertResponse(200);
     $this->assertText('Saved Integer input configuration.', 'Saved settings for integer field.');
+    $this->drupalLogout();
+  }
+
+  /**
+   * Create a data content entity.
+   * 
+   * Creates a content entity of type data_type_test and adds a random
+   * numeric value to the field_integer_input field.
+   */
+  private function createDataEntity() {
+    $this->drupalLogin($this->organizer);
     // Create a data entity using the data type.
     $this->drupalGet('effectiveactivism/data/add/data_type_test');
     $this->assertResponse(200);
     $random_value = rand();
     $this->drupalPostForm(NULL, array(
-      'user_id[0][target_id]' => sprintf('%s (%d)', $this->user->getAccountName(), $this->user->id()),
+      'user_id[0][target_id]' => sprintf('%s (%d)', $this->organizer->getAccountName(), $this->organizer->id()),
       'field_integer_input[0][value]' => $random_value,
     ), t('Save'));
     $this->assertResponse(200);
