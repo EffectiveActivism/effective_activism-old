73430dbc277699582d3337e8c665ec5982db48fd
Roensby <peter@roensby.dk>
2016-06-10 11:11:43 +0200

    Edit and add moved to single class + various fixes

--- modules/ea_import/src/Form/ICalendarForm.php
+++ modules/ea_import/src/Form/ICalendarForm.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains \Drupal\ea_import\Form\AddICalendarForm.
+ * Contains \Drupal\ea_import\Form\ICalendarForm.
  */
 
 namespace Drupal\ea_import\Form;
@@ -18,34 +18,30 @@ use Drupal\Core\Session\AccountInterface;
 /**
  * ICalendar form.
  */
-class AddICalendarForm extends FormBase {
+class ICalendarForm extends FormBase {
 
   /**
    * {@inheritdoc}
    */
   public function getFormID() {
-    return 'ea_import_add_icalendar';
+    return 'ea_import_icalendar';
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, Grouping $grouping = NULL) {
+  public function buildForm(array $form, FormStateInterface $form_state, Grouping $grouping = NULL, $icalendar = NULL) {
     $form = array();
-    $form['import'] = array(
-      '#type' => 'fieldset',
-      '#title' => t('Import'),
-      '#description' => t('Import an ICalendar file from Facebook, Google Calendar or any other ICalendar-compatible website.'),
-    );
-    $form['import']['url'] = array(
+    $form['url'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Url'),
       '#description' => $this->t('The url of the ICalendar file.'),
       '#size' => 20,
       '#maxlength' => 255,
       '#required' => TRUE,
+      '#default_value' => $icalendar !== NULL ? $icalendar->url : NULL,
     );
-    $form['import']['enabled'] = array(
+    $form['enabled'] = array(
       '#type' => 'radios',
       '#title' => $this->t('Enabled'),
       '#description' => $this->t('Enable to continuesly import events from this ICalendar file.'),
@@ -55,46 +51,54 @@ class AddICalendarForm extends FormBase {
         0 => $this->t('Disabled'),
       ),
       '#required' => TRUE,
+      '#default_value' => $icalendar !== NULL ? $icalendar->enabled : 1,
     );
-    $form['import']['uid'] = array(
-      '#type' => 'value',
-      '#value' => \Drupal::currentUser()->id(),
-    );
-    $form['import']['gid'] = array(
-      '#type' => 'value',
-      '#value' => $grouping->id(),
-    );
-    $form['import']['filters'] = array(
+    $form['filters'] = array(
       '#type' => 'details',
       '#title' => t('Filters'),
       '#description' => t('You can limit the import to only events that match a filter criteria.'),
     );
-    $form['import']['filters']['title'] = array(
+    $form['filters']['title'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Title filter'),
       '#description' => $this->t('Only import events that contain this text in their title.'),
       '#size' => 20,
       '#maxlength' => 255,
       '#required' => FALSE,
+      '#default_value' => $icalendar !== NULL ? $icalendar->filter_title : NULL,
     );
-    $form['import']['filters']['description'] = array(
+    $form['filters']['description'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Description filter'),
       '#description' => $this->t('Only import events that contain this text in their description.'),
       '#size' => 20,
       '#maxlength' => 255,
       '#required' => FALSE,
+      '#default_value' => $icalendar !== NULL ? $icalendar->filter_description : NULL,
     );
-    $form['import']['filters']['date'] = array(
+    $form['filters']['date'] = array(
       '#type' => 'date',
       '#title' => $this->t('Date filter'),
       '#description' => $this->t('Only import events that are newer than this date.'),
       '#required' => FALSE,
+      '#default_value' => $icalendar !== NULL ? $icalendar->filter_date : NULL,
     );
-    $form['import']['submit'] = array(
+    $form['submit'] = array(
       '#type' => 'submit',
       '#name' => 'import',
-      '#value' => $this->t('Add'),
+      '#value' => $icalendar !== NULL ? $this->t('Update') : $this->t('Add'),
+    );
+    $form['uid'] = array(
+      '#type' => 'value',
+      '#value' => \Drupal::currentUser()->id(),
+    );
+    $form['gid'] = array(
+      '#type' => 'value',
+      '#value' => $grouping->id(),
+    );
+    $form['iid'] = array(
+      '#type' => 'value',
+      '#value' => $icalendar !== NULL ? $icalendar->iid : NULL,
     );
     return $form;
   }
@@ -152,11 +156,13 @@ class AddICalendarForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Check that the import doesn't exist already.
+    // Uniqueness is determined by url and grouping.
     $existing_icalendar_imports = ICalendarStorage::load(array(
       'url' => $form_state->getValue('url'),
       'gid' => $form_state->getValue('gid'),
     ));
-    if (empty($existing_icalendar_imports)) {
+    $iid = $form_state->getValue('iid');
+    if (empty($existing_icalendar_imports) || $iid === $existing_icalendar_imports[0]->iid) {
       // Save the submitted entry.
       $entry = array(
         'url' => $form_state->getValue('url'),
@@ -165,11 +171,18 @@ class AddICalendarForm extends FormBase {
         'gid' => $form_state->getValue('gid'),
         'filter_title' => $form_state->getValue('title'),
         'filter_description' => $form_state->getValue('description'),
-        'filter_date' => !empty($form_state->getValue('date')) ? $form_state->getValue('date') : 0,
+        'filter_date' => !empty($form_state->getValue('date')) ? strtotime($form_state->getValue('date')) : 0,
       );
-      $return = ICalendarStorage::insert($entry);
-      if ($return) {
-        drupal_set_message(t('Added ICalendar file.'));
+      if ($iid !== NULL) {
+        $entry['iid'] = $iid;
+        if (ICalendarStorage::update($entry)) {
+          drupal_set_message(t('Updated ICalendar file.'));
+        }
+      }
+      else {
+        if (ICalendarStorage::insert($entry)) {
+          drupal_set_message(t('Added ICalendar file.'));
+        }
       }
     }
     else {
