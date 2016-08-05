1d61e4bbf56a2443557eda43030e384f5843bbea
Roensby <peter@roensby.dk>
2016-08-05 13:06:27 +0200

    Added CSV file importer and validation

--- modules/ea_import/src/Form/ImportForm.php
+++ modules/ea_import/src/Form/ImportForm.php
@@ -4,6 +4,7 @@ namespace Drupal\ea_import\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityStorageException;
 
 /**
  * Form controller for Import edit forms.
@@ -19,7 +20,6 @@ class ImportForm extends ContentEntityForm {
     /* @var $entity \Drupal\ea_import\Entity\Import */
     $form = parent::buildForm($form, $form_state);
     $entity = $this->entity;
-
     return $form;
   }
 
@@ -28,21 +28,29 @@ class ImportForm extends ContentEntityForm {
    */
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->entity;
-    $status = parent::save($form, $form_state);
-
-    switch ($status) {
-      case SAVED_NEW:
-        drupal_set_message($this->t('Created the %label Import.', [
-          '%label' => $entity->label(),
-        ]));
-        break;
-
-      default:
-        drupal_set_message($this->t('Saved the %label Import.', [
-          '%label' => $entity->label(),
-        ]));
+    try {
+      $status = parent::save($form, $form_state);
+      switch ($status) {
+        case SAVED_NEW:
+          drupal_set_message($this->t('Created the %label Import.', [
+            '%label' => $entity->label(),
+          ]));
+          break;
+        default:
+          drupal_set_message($this->t('Saved the %label Import.', [
+            '%label' => $entity->label(),
+          ]));
+      }
+      $form_state->setRedirect('entity.import.canonical', ['import' => $entity->id()]);
     }
-    $form_state->setRedirect('entity.import.canonical', ['import' => $entity->id()]);
+    catch (EntityStorageException $exception) {
+      // Custom field validation for configured bundle types is not yet possible.
+      // It is also not yet possible to define bundle types as classes.
+      // Therefore, as a temporary solution, we throw exceptions in a
+      // hook_entity_presave call and catch them here.
+      // This has the downside that the entity form state is discarded
+      // whenever custom field validation fails.
+      drupal_set_message($exception->getMessage(), 'error');
+    }
   }
-
 }
