dbb6d5271b72b1c9d4a604ee418a1c8fc229f0f9
Roensby <peter@roensby.dk>
2016-09-03 19:55:07 +0200

    AFA-6 Implemented ea_permissions module

---
+++ modules/ea_permissions/src/Permission.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\ea_permissions\Permission.
+ * 
+ */
+
+namespace Drupal\ea_permissions;
+
+use Drupal\ea_permissions\Roles;
+use Drupal\ea_groupings\Entity\Grouping;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultAllowed;
+use Drupal\Core\Access\AccessResultForbidden;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides functions to manage permissions.
+ */
+class Permission {
+
+  /**
+   * Creates an allowed access result if the user is organizer of the grouping, denied otherwise.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account for which to check a permission.
+   * @param Grouping $grouping
+   *   The grouping to check relationship for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   Returns an access result.
+   */
+  public static function allowedIfIsOrganizer(AccountInterface $account, Grouping $grouping) {
+    return self::checkPermission($account, $grouping, Roles::ORGANIZER_ROLE);
+  }
+
+  /**
+   * Creates an allowed access result if the user is manager of the grouping, denied otherwise.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account for which to check a permission.
+   * @param Grouping $grouping
+   *   The grouping to check relationship for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   Returns an access result.
+   */
+  public static function allowedIfIsManager(AccountInterface $account, Grouping $grouping) {
+    return self::checkPermission($account, $grouping, Roles::MANAGER_ROLE);
+  }
+
+  /**
+   * Returns access based on grouping and role.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account for which to check a permission.
+   * @param \Drupal\ea_grouping\Entity\Grouping $grouping
+   *   The grouping to check relationship for.
+   * @param string $role
+   *   The role to check for.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   Returns an access result.
+   */
+  private static function checkPermission(AccountInterface $account, Grouping $grouping, String $role) {
+    // Allow access for administrators.
+    foreach ($account->getRoles() as $userRole) {
+      if ($userRole === 'administrator') {
+        return new AccessResultAllowed();
+      }
+    }
+    // Default response is to deny access.
+    $access = new AccessResultForbidden();
+    // Determine access based on role.
+    switch ($role) {
+      case Roles::ORGANIZER_ROLE :
+        if (in_array($account->id(), $grouping->get('organizers')->getValue())) {
+          $access = new AccessResultAllowed();
+        }
+      case Roles::MANAGER_ROLE :
+        if (in_array($account->id(), $grouping->get('managers')->getValue())) {
+          $access = new AccessResultAllowed();
+        }
+    }
+    return $access;
+  }
+}
---
+++ modules/ea_permissions/src/Roles.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\ea_permissions\Roles.
+ * 
+ */
+
+namespace Drupal\ea_permissions;
+
+class Roles {
+
+  /**
+   * Defines the organizer role.
+   */
+  const ORGANIZER_ROLE = 'organizer';
+
+  /**
+   * Defines the organizer permissions.
+   */
+  const ORGANIZER_PERMISSIONS = array(
+    // People permissions.
+    'add person entities',
+    'delete person entities',
+    'edit person entities',
+    'view published person entities',
+    // Grouping permissions.
+    'view published grouping entities',
+    // Activity permissions.
+    'add activity entities',
+    'delete activity entities',
+    'edit activity entities',
+    'view published activity entities',
+    // Data permissions.
+    'add data entities',
+    'edit data entities',
+    'view data entities',
+    // Event permissions.
+    'add event entities',
+    'delete event entities',
+    'edit event entities',
+    'view published event entities',
+    'view unpublished event entities',
+    // Event repeater permissions.
+    'add event repeater entities',
+    'delete event repeater entities',
+    'edit event repeater entities',
+    'view event repeater entities',
+    // Task permissions.
+    'add task entities',
+    'delete task entities',
+    'edit task entities',
+    'view published task entities',
+    'view unpublished task entities',
+    // Import permissions.
+    'access import overview',
+    'add import entities',
+    'edit import entities',
+    'view published task entities',
+    'view unpublished task entities',
+  );
+
+  /**
+   * Defines the manager role.
+   */
+  const MANAGER_ROLE = 'manager';
+
+  /**
+   * Defines the manager permissions.
+   */
+  const MANAGER_PERMISSIONS = array(
+    // People permissions.
+    'add person entities',
+    'delete person entities',
+    'edit person entities',
+    'view published person entities',
+    // Grouping permissions.
+    'add grouping entities',
+    'edit grouping entities',
+    'view published grouping entities',
+    'view unpublished grouping entities',
+    // Activity permissions.
+    'administer activity entities',
+    'add activity entities',
+    'delete activity entities',
+    'edit activity entities',
+    'view published activity entities',
+    // Data permissions.
+    'administer data entities',
+    'administer data fields',
+    'administer data display',
+    // Event permissions.
+    'add event entities',
+    'delete event entities',
+    'edit event entities',
+    'view published event entities',
+    'view unpublished event entities',
+    // Event repeater permissions.
+    'add event repeater entities',
+    'delete event repeater entities',
+    'edit event repeater entities',
+    'view event repeater entities',
+    // Task permissions.
+    'add task entities',
+    'delete task entities',
+    'edit task entities',
+    'view published task entities',
+    'view unpublished task entities',
+    // Import permissions.
+    'access import overview',
+    'add import entities',
+    'edit import entities',
+    'view published task entities',
+    'view unpublished task entities',
+  );
+}
