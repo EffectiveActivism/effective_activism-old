9dcb14dad2331c46aea5697abf1f18e381e3515d
Roensby <peter@roensby.dk>
2016-09-04 17:55:20 +0200

    AFA-6 Added travis script to patch inline entity form

---
+++ travis/inline_entity_form.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Add statement to see that this is running in Travis CI.
+echo "running travis/inline_entity_form.sh"
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# The first time this is run, it will install Drupal.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Change to the Drupal module directory
+cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
+
+# Manually download inline entity form and apply patch from https://www.drupal.org/node/2569193.
+git clone --branch 8.x-1.x-dev https://git.drupal.org/project/inline_entity_form.git
+cd inline_entity_form
+wget -q -O - https://www.drupal.org/files/2569193-23.patch | git apply -
