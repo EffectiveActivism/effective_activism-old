028e1c30f5a0311393c2a8266b6a97930d3d2f39
Roensby <peter@roensby.dk>
2016-09-08 16:41:56 +0200

    AFA-20 Added validation check for unique participants in events

--- modules/ea_events/src/Form/EventForm.php
+++ modules/ea_events/src/Form/EventForm.php
@@ -30,6 +30,38 @@ class EventForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $participants = $form_state->getValue('participants');
+    if (isset($participants['form']['inline_entity_form'])) {
+      $mobile_phone_number = $participants['form']['inline_entity_form']['mobile_phone_number'];
+      $email_address = $participants['form']['inline_entity_form']['email_address'];
+      // Check if either mobile phone number or e-mail is set.
+      if (empty($mobile_phone_number[0]['value']) && empty($email_address[0]['value'])) {
+        $form_state->setErrorByName($form_state->getTriggeringElement(), $this->t('Please add at least one contact method.'));
+      }
+      // Check if e-mail or phone number exists in any grouping related to host grouping.
+      if (isset($this->entity->grouping->entity)) {
+        $host = $this->entity->grouping->entity;
+        $groupings = $host->getRelatives();
+        foreach ($groupings as $grouping) {
+          // Check each member of the grouping.
+          foreach ($grouping->get('members')->referencedEntities() as $member) {
+            if ($mobile_phone_number[0]['value'] === $member->get('mobile_phone_number')->value) {
+              $form_state->setError($form_state->getTriggeringElement(), $this->t('This mobile phone number already exists.'));
+            }
+            if ($email_address[0]['value'] === $member->get('email_address')->value) {
+              $form_state->setError($form_state->getTriggeringElement(), $this->t('This e-mail address already exists.'));
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->entity;
     $status = parent::save($form, $form_state);
