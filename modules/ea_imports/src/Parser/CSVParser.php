43c0843c2a2c8edde26cab8b9595c4a27fd7f0bf
Roensby <peter@roensby.dk>
2016-09-24 18:40:52 +0200

    AFA-26 Refactored CSV import and added support for entity references

--- modules/ea_imports/src/Parser/CSVParser.php
+++ modules/ea_imports/src/Parser/CSVParser.php
@@ -2,7 +2,15 @@
 
 namespace Drupal\ea_imports\Parser;
 
+use Drupal\ea_imports\Parser\ParserValidationException;
+use Drupal\ea_groupings\Entity\Grouping;
+use Drupal\ea_events\Entity\Event;
+use Drupal\ea_events\Entity\EventRepeater;
+use Drupal\ea_data\Entity\Data;
+use Drupal\ea_activities\Entity\Activity;
 use Drupal\file\Entity\File;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityStorageException;
 
 /**
  * Parses ICalendar.
@@ -17,81 +25,455 @@ class CSVParser {
   private $file;
 
   /**
+   * Parent grouping.
+   *
+   * @var Grouping
+   */
+  private $grouping;
+
+  /**
+   * The current line number.
+   * 
+   * @var int
+   */
+  private $line;
+
+  /**
+   * The current column number.
+   * 
+   * @var int
+   */
+  private $column;
+
+  /**
+   * The event entity field definitions.
+   *
+   * @var array
+   */
+  private $fieldDefinitions;
+  
+  /**
+   * The parsed data.
+   * 
+   * $var array
+   */
+  private $data;
+
+  /**
+   * Any validation error message.
+   * 
+   * $var array
+   */
+  private $errorMessage;
+
+  /**
    * Creates the CSVParser Object.
    *
    * @param string $file
-   *   An iCal file.
+   *   A CSV file.
+   * @param int $gid
+   *   The parent grouping id of the events.
    */
-  public function __construct($file) {
+  public function __construct($file, $gid) {
     if (empty($file)) {
       return FALSE;
     }
     // Load file entity.
     $csv_file = File::load($file);
     $this->file = $csv_file;
+    // Load the grouping.
+    $this->grouping = Grouping::load($gid);
+    // Set basefield definitions.
+    $this->fieldDefinitions = $this->getFieldDefinitions('event');
     return $this;
   }
 
   /**
-   * Return parsed events.
-   *
-   * @return array
-   *   List of event data.
+   * {@inheritdoc}
    */
-  public function getEvents() {
-    $events = [];
-    if (($handle = fopen($this->file->getFileUri(), "r")) !== FALSE) {
-      $row = 0;
-      while (($data = fgetcsv($handle)) !== FALSE) {
-        $row++;
-        // Skip headers.
-        if ($row === 1) {
-          continue;
-        }
-        $events[] = array(
-          'title' => $data[0],
-          'description' => $data[1],
-          'start_date' => date(DATETIME_DATETIME_STORAGE_FORMAT, $data[2]),
-          'end_date' => date(DATETIME_DATETIME_STORAGE_FORMAT, $data[3]),
-          'location' => [
-            'address' => $data[4],
-            'extra_information' => $data[5],
-          ],
-        );
-      }
-      fclose($handle);
+  public function validate() {
+    try {
+      $this->validateColumnNames();
+      $this->validateData();
     }
-    return $events;
+    catch (ParserValidationException $exception) {
+      switch ($exception->getMessage()) {
+        case WRONG_COLUMN_COUNT:
+          $this->errorMessage = t('The CSV file does not have the correct number of columns. Column names should be "@column_names_format"', ['@column_names_format' => implode(', ', array_keys($this->fieldDefinitions))]);
+          break;
+
+        case INVALID_HEADERS:
+          $this->errorMessage = t('The CSV file does not contain valid column names. Column names should be "@column_names_format"', ['@column_names_format' => implode(', ', array_keys($this->fieldDefinitions))]);
+          break;
+
+        case REQUIRED_VALUE:
+          $this->errorMessage = t('The CSV file contains a required value that is missing at line @line, column @column.', ['@line' => $exception->getDataLine(), '@column' => $exception->getDataColumn()]);
+          break;
+
+        case WRONG_INT_FORMAT:
+          $this->errorMessage = t('The CSV file contains a value that is not formatted properly at line @line, column @column.', ['@line' => $exception->getDataLine(), '@column' => $exception->getDataColumn()]);
+          break;
+
+        case WRONG_DATE_FORMAT:
+          $this->errorMessage = t('The CSV file contains a date that is not formatted properly at line @line, column @column.', ['@line' => $exception->getDataLine(), '@column' => $exception->getDataColumn()]);
+          break;
+
+        case WRONG_LOCATION_FORMAT:
+          $this->errorMessage = $this->errorMessage = t('The CSV file contains a location that is not formatted properly at line @line, column @column. Location should be formatted as @valid_format', [
+            '@line' => $exception->getDataLine(),
+            '@column' => $exception->getDataColumn(),
+            '@valid_format' => $exception->getAdditionalInformation(),
+          ]);
+          break;
+
+        case WRONG_ENTITY_FORMAT:
+          if (!empty($exception->getAdditionalInformation())) {
+            $this->errorMessage = t('The CSV file contains a value that is not formatted properly at line @line, column @column. Value should be formatted as @valid_format', [
+              '@line' => $exception->getDataLine(),
+              '@column' => $exception->getDataColumn(),
+              '@valid_format' => $exception->getAdditionalInformation(),
+            ]);
+          }
+          else {
+            $this->errorMessage = t('The CSV file contains a value that is not formatted properly at line @line, column @column.', [
+              '@line' => $exception->getDataLine(),
+              '@column' => $exception->getDataColumn(),
+            ]);
+          }
+          break;
+
+        case PERMISSION_DENIED:
+          $this->errorMessage('The CSV file contains a row with an activity that is not permissable for this grouping at line @line, column @column', ['@line' => $this->line, '@column' => $this->column]);
+          break;
+
+        default:
+          $this->errorMessage = t('The CSV file failed to validate.');
+      }
+      return FALSE;
+    }
+    return TRUE;
   }
 
   /**
-   * Validate headers.
-   *
-   * @return bool
-   *   Whether or not CSV headers are valid.
+   * {@inheritdoc}
    */
-  public function validateHeaders() {
-    $isValid = FALSE;
+  public function import() {
     if (($handle = fopen($this->file->getFileUri(), "r")) !== FALSE) {
-      $titles = fgetcsv($handle);
-      fclose($handle);
-      if (
-        count($titles) < 6 ||
-        empty($titles) ||
-        $titles[0] !== 'title' ||
-        $titles[1] !== 'description' ||
-        $titles[2] !== 'date_start' ||
-        $titles[3] !== 'date_end' ||
-        $titles[4] !== 'location' ||
-        $titles[5] !== 'location_extra_information'
-      ) {
-        $isValid = FALSE;
+      $this->line = 0;
+      while (($row = fgetcsv($handle)) !== FALSE) {
+        // Skip headers.
+        if ($this->line === 0) {
+          $this->line++;
+          continue;
+        }
+        $columnNames = array_keys($this->fieldDefinitions);
+        $values = [];
+        foreach ($columnNames as $column => $name) {
+          $this->column = $column;
+          switch ($this->fieldDefinitions[$name]->getType()) {
+            case 'datetime':
+              // Format datetime object.
+              $value = date(DATETIME_DATETIME_STORAGE_FORMAT, $row[$column]);
+              break;
+
+            case 'location':
+              // Format location field.
+              $value = json_decode($row[$column], TRUE);
+              break;
+
+            case 'entity_reference':
+              // Unpack entity reference.
+              $value = $this->unpackEntityReference($name, $row[$column], TRUE);
+              break;
+
+            default:
+              $value = $row[$column];
+          }
+          if (!empty($value)) {
+            $values[$name] = $value;
+          }
+        }
+        // Add parent grouping to event.
+        $values['grouping'] = $this->grouping->id();
+        // Create event.
+        try {
+          $event = Event::create($values);
+          $event->save();
+        }
+        catch (EntityStorageException $exception) {
+          drupal_set_message(t('The CSV file contains a row that failed to import at line @line', ['@line' => $this->line]), 'error');
+        }
+        $this->line++;
       }
-      else {
-        $isValid = TRUE;
+      fclose($handle);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getErrorMessage() {
+    return $this->errorMessage;
+  }
+
+  /**
+   * Filters definitions from the entity basefield definitions.
+   * 
+   * @param string @type
+   *   The entity type.
+   * @param string @bundle
+   *   The entity bundle
+   * 
+   * @return array
+   *   An array of field definitions for the entity type.
+   */
+  private function getFieldDefinitions($type, $bundle = NULL) {
+    if (empty($bundle)) {
+      // For entities without bundles, set the bundle to the type.
+      $bundle = $type;
+    }
+    $fieldDefinitions = \Drupal::entityManager()->getFieldDefinitions($type, $bundle);
+    // Do not include standard fields.
+    unset($fieldDefinitions['id']);
+    unset($fieldDefinitions['uuid']);
+    unset($fieldDefinitions['user_id']);
+    unset($fieldDefinitions['status']);
+    unset($fieldDefinitions['langcode']);
+    unset($fieldDefinitions['created']);
+    unset($fieldDefinitions['changed']);
+    // Do not include grouping field as this is set on the Import entity.
+    unset($fieldDefinitions['grouping']);
+    return $fieldDefinitions;
+  }
+
+  /**
+   * Validates column names.
+   */
+  private function validateColumnNames() {
+    if (($handle = fopen($this->file->getFileUri(), "r")) !== FALSE) {
+      $column_names = fgetcsv($handle);
+      fclose($handle);
+      if (count($column_names) < count($this->fieldDefinitions)) {
+        throw new ParserValidationException(WRONG_COLUMN_COUNT);
+      }
+      elseif ($column_names !== array_keys($this->fieldDefinitions)) {
+        throw new ParserValidationException(INVALID_HEADERS);
       }
     }
-    return $isValid;
+    else {
+      throw new ParserValidationException();
+    }
+  }
+
+  /**
+   * Validate data.
+   */
+  private function validateData() {
+    if (($handle = fopen($this->file->getFileUri(), "r")) !== FALSE) {
+      $this->line = 0;
+      // Run through every row to validate it.
+      while (($row = fgetcsv($handle)) !== FALSE) {
+        // Skip column names.
+        if ($this->line === 0) {
+          $this->line++;
+          continue;
+        }
+        $this->column = 0;
+        foreach ($row as $cell) {
+          // Validate by field definition.
+          $this->validateValue($cell, $this->fieldDefinitions[array_keys($this->fieldDefinitions)[$this->column]]);
+          $this->column++;
+        }
+        $this->line++;
+      }
+      fclose($handle);
+    }
+    else {
+      throw new ParserValidationException();
+    }
+  }
+
+  /**
+   * Validate a value by the corresponding field definition.
+   * 
+   * @param string $value
+   *   A value to validate.
+   * @param BaseFieldDefinition $fieldDefinition
+   *   The field definition to validate against.
+   */
+  private function validateValue($value, $fieldDefinition = NULL) {
+    if (empty($fieldDefinition)) {
+      throw new ParserValidationException(INVALID_HEADERS);
+    }
+    if (empty($value) && $fieldDefinition->isRequired()) {
+      // Allow event repeater value to be empty.
+      if ($fieldDefinition->getName() !== 'event_repeater') {
+        throw new ParserValidationException(REQUIRED_VALUE, $this->line + 1, $this->column + 1);
+      }
+    }
+    switch ($fieldDefinition->getType()) {
+      case 'integer':
+        if (!is_integer($value)) {
+          throw new ParserValidationException(WRONG_INT_FORMAT, $this->line + 1, $this->column + 1);
+        }
+        break;
+
+      case 'datetime':
+        if (!is_numeric($value)) {
+          throw new ParserValidationException(WRONG_DATE_FORMAT, $this->line + 1, $this->column + 1);
+        }
+        break;
+
+      case 'location':
+        if (!empty($value)) {
+          $location_format = ['address', 'extra_information'];
+          $location = json_decode($value, TRUE);
+          if (empty($location) || !is_array($location)) {
+            throw new ParserValidationException(WRONG_LOCATION_FORMAT, $this->line + 1, $this->column + 1, json_encode($location_format));
+          }
+          if (array_keys($location) !== $location_format) {
+            throw new ParserValidationException(WRONG_LOCATION_FORMAT, $this->line + 1, $this->column + 1, json_encode($location_format));
+          }
+        }
+        break;
+
+      case 'entity_reference':
+        if (!empty($value)) {
+          // Load the base field definitions of the referenced entity type.
+          $referecedEntityType = $fieldDefinition->getItemDefinition()->getSetting('target_type');
+          if ($referecedEntityType === 'activity') {
+            // This is a special case: We need to ensure that the
+            // data type is defined in the json array and set
+            // the basefield definitions to match the specific data type.
+            // Verify the json.
+            // Verify the decoded array.
+            $decoded_json = json_decode($value, TRUE);
+            if (empty($decoded_json)) {
+              throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1);
+            }
+            // Validate array against field definitions.
+            foreach ($decoded_json as $activityEntityType => $dataEntityValues) {
+              if (empty($activityEntityType) || !is_string($activityEntityType)) {
+                throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1);
+              }
+              $activityEntityTypeFieldDefinitions = $this->getFieldDefinitions('activity', $activityEntityType);
+              if (!is_array($dataEntityValues)) {
+                throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($activityEntityTypeFieldDefinitions)]));
+              }
+              // Set data entity type.
+              $dataEntityValues = ['type' => str_replace('field_', '', array_keys($dataEntityValues)[0])] + $dataEntityValues;
+              if (array_keys($dataEntityValues) !== array_keys($activityEntityTypeFieldDefinitions)) {
+                throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($activityEntityTypeFieldDefinitions)]));
+              }
+            }
+          }
+          else {
+            $referencedEntityFieldDefinitions = $this->getFieldDefinitions($referecedEntityType);
+            // Verify the json.
+            if (!preg_match('/[^,:{}\\[\\]0-9.\\-+Eaeflnr-u \\n\\r\\t]/', preg_replace('/"(\\.|[^"\\\\])*"/', '', $value))) {
+              throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($referencedEntityFieldDefinitions)]));
+            }
+            // Verify the decoded array.
+            $decoded_json = json_decode($value, TRUE);
+            if (!is_array($decoded_json) || empty($decoded_json)) {
+              throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($referencedEntityFieldDefinitions)]));
+            }
+            // Validate array against field definitions.
+            foreach ($decoded_json as $entityValues) {
+              if (!is_array($entityValues)) {
+                throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($referencedEntityFieldDefinitions)]));
+              }
+              if (array_keys($entityValues) !== array_keys($referencedEntityFieldDefinitions)) {
+                throw new ParserValidationException(WRONG_ENTITY_FORMAT, $this->line + 1, $this->column + 1, json_encode([array_keys($referencedEntityFieldDefinitions)]));
+              }
+            }
+          }
+        }
+        break;
+
+      default:
+    }
+  }
+
+  /**
+   * Unpacks a json-formatted array into an entity.
+   * 
+   * @param string $type
+   *   The entity type to expect.
+   * @param string $json
+   *   A json-formatted string defining an entity.
+   * @param bool $save
+   *   Whether or not the entity should be saved.
+   * 
+   * @return array
+   *   The unpacked entity id(s).
+   */
+  private function unpackEntityReference($type, $json = NULL, $save = FALSE) {
+    $ids = array();
+    if (!empty($json)) {
+      $array = json_decode($json, TRUE);
+      try {
+        switch ($type) {
+          case 'tasks':
+            $taskEntity = Task::create($array);
+            if ($taskEntity && $save) {
+              $taskEntity->save();
+              $ids[] = $taskEntity-id();
+              break;
+            }
+            break;
+
+          case 'event_repeater':
+            $eventRepeaterEntity = EventRepeater::create($array);
+            if ($eventRepeaterEntity && $save) {
+              $eventRepeaterEntity->save();
+              $ids[] = $eventRepeaterEntity-id();
+            }
+            break;
+
+          case 'participants':
+            foreach ($array as $item) {
+              $participantEntity = Participant::create($item);
+              if ($participantEntity && $save) {
+                $participantEntity->save();
+                $ids[] = $participantEntity->id();
+              }
+            }
+            break;
+
+          case 'activities':
+            foreach ($array as $activityType => $dataValues) {
+              // Get data types and save corresponding data entities.
+              $dataIds = [];
+              foreach ($dataValues as $dataField => $dataValue) {
+                $dataEntity = Data::create([
+                  'type' => str_replace('field_', '', $dataField),
+                  $dataField => $dataValue,
+                ]);
+                if ($dataEntity && $save) {
+                  $dataEntity->save();
+                  $dataIds[] = $dataEntity->id();
+                }
+              }
+              $array['type'] = $activityType;
+              $array[$dataField] = $dataIds;
+              $activityEntity = Activity::create($array);
+              if ($activityEntity && $save) {
+                $activityEntity->save();
+                $ids[] = $activityEntity->id();
+              }
+            }
+            break;
+
+          default:
+        }
+      }
+      catch (EntityStorageException $exception) {
+        drupal_set_message($exception->getMessage());
+        drupal_set_message(t('The CSV file contains a row that failed to parse a value at line @line, column @column', ['@line' => $this->line, '@column' => $this->column]), 'error');
+      }
+    }
+    return $ids;
   }
 
 }
--- modules/ea_imports/src/Tests/CSVImportTest.php
+++ modules/ea_imports/src/Tests/CSVImportTest.php
@@ -4,6 +4,10 @@ namespace Drupal\ea_imports\Tests;
 
 use Drupal\ea_permissions\Roles;
 use Drupal\ea_groupings\Entity\Grouping;
+use Drupal\ea_data\Entity\DataType;
+use Drupal\ea_activities\Entity\ActivityType;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
 
 /**
  * Function tests for ea_imports CSV import entity type.
@@ -13,6 +17,8 @@ use Drupal\ea_groupings\Entity\Grouping;
 class CSVImportTest extends ImportWebTestBase {
 
   private $grouping;
+
+  private $dataType;
 
   private $organizer;
 
@@ -27,15 +33,9 @@ class CSVImportTest extends ImportWebTestBase {
     parent::setUp();
     $this->manager = $this->drupalCreateUser(Roles::MANAGER_PERMISSIONS);
     $this->organizer = $this->drupalCreateUser(Roles::ORGANIZER_PERMISSIONS);
-    // Create grouping.
-    $this->grouping = Grouping::create(array(
-      'user_id' => $this->manager->id(),
-      'name' => ImportWebTestBase::GROUPNAME,
-      'timezone' => \Drupal::config('system.date')->get('timezone.default'),
-      'managers' => $this->manager->id(),
-      'organizers' => $this->organizer->id(),
-    ));
-    $this->grouping->save();
+    $this->grouping = $this->createGrouping();
+    $this->dataType = $this->createLeafletDataType();
+    $this->createActivityType();
     // Create CSV file.
     $data = file_get_contents($this->container->get('file_system')->realpath(drupal_get_path('module', 'ea_imports') . '/src/Tests/sample.csv'));
     $this->CSVFile = file_save_data($data, 'public://sample.csv', FILE_EXISTS_REPLACE);
@@ -62,7 +62,104 @@ class CSVImportTest extends ImportWebTestBase {
     ), t('Save'));
     $this->assertResponse(200);
     $this->assertText('Created the import.', 'Added a new import entity.');
-    $this->assertText('One event imported', 'Successfully imported event');
   }
 
+  /**
+   * Create grouping.
+   * 
+   * @return Grouping
+   *   The created grouping.
+   */
+  private function createGrouping() {
+    $grouping = Grouping::create(array(
+      'user_id' => $this->manager->id(),
+      'name' => ImportWebTestBase::GROUPNAME,
+      'timezone' => \Drupal::config('system.date')->get('timezone.default'),
+      'managers' => $this->manager->id(),
+      'organizers' => $this->organizer->id(),
+    ));
+    $grouping->save();
+    return $grouping;
+  }
+
+  /**
+   * Create leaflet data type.
+   * 
+   * @return DataType
+   *   The created leaflet data type.
+   */
+  private function createLeafletDataType() {
+    $bundle = 'leaflets';
+    $label = 'Leaflets';
+    $entity_type_id = 'data';
+    $field_name = 'field_leaflets';
+    // Create data type.
+    $dataType = DataType::create(array(
+      'id' => $bundle,
+      'label' => $label,
+    ));
+    $dataType->save();
+    // Add an integer field to the data type.
+    $field_storage = FieldStorageConfig::loadByName($entity_type_id, $field_name);
+    if (empty($field_storage)) {
+      $field_storage = FieldStorageConfig::create(array(
+        'field_name' => $field_name,
+        'entity_type' => $entity_type_id,
+        'type' => 'integer',
+        'cardinality' => 1,
+        'module' => 'core',
+        'settings' => ['min' => '0'],
+      ))->save();
+    }
+    $field = FieldConfig::loadByName($entity_type_id, $bundle, $field_name);
+    if (empty($field)) {
+      $field = FieldConfig::create(array(
+        'field_name' => $field_name,
+        'entity_type' => $entity_type_id,
+        'bundle' => $bundle,
+        'label' => $label,
+      ));
+      $field
+        ->setRequired(TRUE)
+        ->save();
+    }
+    // Form display settings for field_integer_input.
+    entity_get_form_display($entity_type_id, $bundle, 'default')
+      ->setComponent($field_name, array(
+        'type' => 'number',
+      ))
+      ->save();
+    // View display settings for field_integer_input.
+    entity_get_display($entity_type_id, $bundle, 'default')
+      ->setComponent($field_name, array(
+        'type' => 'number_integer',
+      ))
+      ->save();
+    return $dataType;
+  }
+
+  /**
+   * Create an activity type.
+   */
+  private function createActivityType() {
+    $this->drupalLogin($this->manager);
+    $this->drupalGet('effectiveactivism/activity-types');
+    $this->assertResponse(200);
+    $this->drupalGet('effectiveactivism/activity-types/add');
+    $this->assertResponse(200);
+    // Create an activity type.
+    $this->drupalPostAjaxForm(NULL, [
+      'organization' => $this->grouping->id(),
+    ], 'organization');
+    $this->drupalPostForm(NULL, array(
+      'label' => 'Leafleting',
+      'id' => 'leafleting',
+      'description' => 'Sample activity',
+      'data_types' => sprintf('%s (%s)', 'Leaflets', 'leaflets'),
+      'organization' => $this->grouping->id(),
+      'groupings[]' => [$this->grouping->id()],
+    ), t('Save'));
+    $this->assertResponse(200);
+    $this->assertText('Created the Leafleting Activity type.', 'Added a new activity type.');
+  }
 }
