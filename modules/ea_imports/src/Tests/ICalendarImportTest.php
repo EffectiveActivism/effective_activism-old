0c2bbae87955cad78a9443383ef767a5aa5b436e
Roensby <peter@roensby.dk>
2016-09-29 17:24:12 +0200

    AFA-8 Disable testing for ICalendar import until it can handle revisions

--- modules/ea_imports/src/Tests/ICalendarImportTest.php
+++ modules/ea_imports/src/Tests/ICalendarImportTest.php
@@ -7,8 +7,6 @@ use Drupal\ea_groupings\Entity\Grouping;
 
 /**
  * Function tests for ea_imports ICalendar import entity type.
- *
- * @group effective_activism
  */
 class ICalendarImportTest extends ImportWebTestBase {
 
