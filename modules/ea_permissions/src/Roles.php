ce8a91157b1bb67e2d8e2f51d649848269422ac8
Roensby <peter@roensby.dk>
2016-10-06 19:47:07 +0200

    AFA-42 Remove old, unused permissions

--- modules/ea_permissions/src/Roles.php
+++ modules/ea_permissions/src/Roles.php
@@ -16,38 +16,8 @@ class Roles {
    * Defines the organizer permissions.
    */
   const ORGANIZER_PERMISSIONS = array(
-    // People permissions.
-    'add person entities',
-    'edit person entities',
-    'view published person entities',
-    // Grouping permissions.
-    'view published grouping entities',
-    // Result permissions.
-    'add result entities',
-    'edit result entities',
-    'view published result entities',
-    // Data permissions.
-    'add data entities',
-    'edit data entities',
-    'view data entities',
-    'administer data entities',
-    // Event permissions.
-    'add event entities',
-    'view published event entities',
-    // Event repeater permissions.
-    'view event repeater entities',
-    'edit event repeater entities',
-    'add event repeater entities',
     // Location field permissions.
     'use location autocomplete',
-    // Task permissions.
-    'add task entities',
-    'edit task entities',
-    'view published task entities',
-    'view unpublished task entities',
-    // Import permissions.
-    'add import entities',
-    'view import entities',
     // Use the administration toolbar.
     'access toolbar',
   );
@@ -61,40 +31,8 @@ class Roles {
    * Defines the manager permissions.
    */
   const MANAGER_PERMISSIONS = array(
-    // People permissions.
-    'add person entities',
-    'edit person entities',
-    'view published person entities',
-    // Grouping permissions.
-    'add grouping entities',
-    'view published grouping entities',
-    // Result permissions.
-    'administer result entities',
-    'add result entities',
-    'edit result entities',
-    'view published result entities',
-    // Data permissions.
-    'add data entities',
-    'edit data entities',
-    'view data entities',
-    'administer data entities',
-    // Event permissions.
-    'add event entities',
-    'view published event entities',
-    // Event repeater permissions.
-    'view event repeater entities',
-    'edit event repeater entities',
-    'add event repeater entities',
     // Location field permissions.
     'use location autocomplete',
-    // Task permissions.
-    'add task entities',
-    'edit task entities',
-    'view published task entities',
-    'view unpublished task entities',
-    // Import permissions.
-    'add import entities',
-    'view import entities',
     // Use the administration toolbar.
     'access toolbar',
   );
