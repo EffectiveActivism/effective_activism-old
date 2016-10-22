8cde658191359f3da1ebd162f720074c6ee29979
Roensby <peter@roensby.dk>
2016-10-22 14:35:10 +0200

    AFA-58 Patch in https://www.drupal.org/node/2569193 no longer applies

--- .travis_scripts/inline_entity_form.sh
+++ .travis_scripts/inline_entity_form.sh
@@ -13,8 +13,5 @@ drupal_ti_ensure_drupal
 # Change to the Drupal module directory
 cd "$DRUPAL_TI_DRUPAL_DIR/modules"
 
-# Manually download inline entity form and apply patch from https://www.drupal.org/node/2569193.
+# Manually download dev verison of inline_entity_form.
 git clone --branch 8.x-1.x https://git.drupal.org/project/inline_entity_form.git
-cd inline_entity_form
-wget -q https://www.drupal.org/files/issues/2569193-23.patch
-git apply -v 2569193-23.patch
