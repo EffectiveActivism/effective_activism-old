5c78db04b3b856db9e17a003d3e15106635e3e71
Roensby <peter@roensby.dk>
2016-10-22 12:01:28 +0200

    AFA-58 Re-include ICalender test in test group

--- modules/ea_imports/src/Tests/ICalendarImportTest.php
+++ modules/ea_imports/src/Tests/ICalendarImportTest.php
@@ -8,7 +8,7 @@ use Drupal\ea_groupings\Entity\Grouping;
 /**
  * Function tests for ea_imports ICalendar import entity type.
  *
- * @group no_group
+ * @group effective_activism
  */
 class ICalendarImportTest extends ImportWebTestBase {
 
