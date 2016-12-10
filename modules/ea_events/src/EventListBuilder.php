8183a29a2d53b12179aedf9cc80009c44eb5ccc7
Peter Rønsby <pr@activeforanimals.com>
2016-12-10 18:02:02 +0100

    AFA-102 Beautified event list

--- modules/ea_events/src/EventListBuilder.php
+++ modules/ea_events/src/EventListBuilder.php
@@ -20,8 +20,11 @@ class EventListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['id'] = $this->t('Event ID');
-    $header['name'] = $this->t('Name');
+    $header['date'] = $this->t('Date');
+    $header['start_time'] = $this->t('Start time');
+    $header['end_time'] = $this->t('End time');
+    $header['title'] = $this->t('Title');
+    $header['grouping'] = $this->t('Group');
     return $header + parent::buildHeader();
   }
 
@@ -30,13 +33,25 @@ class EventListBuilder extends EntityListBuilder {
    */
   public function buildRow(EntityInterface $entity) {
     if ($entity->access('view', \Drupal::currentUser())) {
-      $row['id'] = $entity->id();
-      $row['name'] = $this->l(
-        $entity->id(),
+      $row['date'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('d/m Y');
+      $row['start_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('H:i');
+      $row['end_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('end_date')->value)->format('H:i');
+      $row['title'] = $this->l(
+        empty($entity->get('title')->value) ? $this->t('View') : $entity->get('title')->value,
         new Url(
-          'entity.event.edit_form', array(
+          'entity.event.canonical',
+          [
             'event' => $entity->id(),
-          )
+          ]
+        )
+      );
+      $row['grouping'] = $this->l(
+        $entity->get('grouping')->entity->get('name')->value,
+        new Url(
+          'entity.grouping.canonical',
+          [
+            'grouping' => $entity->get('grouping')->entity->id(),
+          ]
         )
       );
       return $row + parent::buildRow($entity);
