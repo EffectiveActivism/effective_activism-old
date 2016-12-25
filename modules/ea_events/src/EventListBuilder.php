8eefba75891b01fb177b21d1e5a91b6918b0b914
Peter Rønsby <pr@activeforanimals.com>
2016-12-25 13:44:08 +0100

    AFA-106 Updated listbuilder functions

--- modules/ea_events/src/EventListBuilder.php
+++ modules/ea_events/src/EventListBuilder.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Routing\LinkGeneratorTrait;
 use Drupal\Core\Url;
+use Drupal\ea_groupings\Entity\Grouping;
 
 /**
  * Defines a class to build a listing of Event entities.
@@ -32,19 +33,36 @@ class EventListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    if ($entity->access('view', \Drupal::currentUser())) {
-      $row['grouping'] = $this->l(
-        $entity->get('grouping')->entity->getTitle(),
-        new Url('entity.grouping.canonical', [
-          'grouping' => $entity->get('grouping')->entity->id(),
-        ])
-      );
-      $row['date'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('d/m Y');
-      $row['start_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('H:i');
-      $row['end_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('end_date')->value)->format('H:i');
-      $row['status'] = $entity->isPublished() ? $this->t('Published') : $this->t('Unpublished');
-      return $row + parent::buildRow($entity);
+    $row['grouping'] = $this->l(
+      $entity->get('grouping')->entity->getTitle(),
+      new Url('entity.grouping.canonical', [
+        'grouping' => $entity->get('grouping')->entity->id(),
+      ])
+    );
+    $row['date'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('d/m Y');
+    $row['start_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('start_date')->value)->format('H:i');
+    $row['end_time'] = \DateTime::createFromFormat('Y-m-d\TH:i:s', $entity->get('end_date')->value)->format('H:i');
+    $row['status'] = $entity->isPublished() ? $this->t('Published') : $this->t('Unpublished');
+    return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+    $query = $this->getStorage()->getQuery()
+      ->sort($this->entityType->getKey('id'));
+    // Filter entities for non-admin users.
+    if (\Drupal::currentUser()->id() !== '1') {
+      $grouping_ids = Grouping::getAllGroupingsByUser(\Drupal::currentUser(), FALSE);
+      $query->condition('grouping', $grouping_ids, 'IN');
     }
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    $result = $query->execute();
+    return $result;
   }
 
 }
--- modules/ea_groupings/src/GroupingListBuilder.php
+++ modules/ea_groupings/src/GroupingListBuilder.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Routing\LinkGeneratorTrait;
 use Drupal\Core\Url;
+use Drupal\ea_groupings\Entity\Grouping;
 
 /**
  * Defines a class to build a listing of Grouping entities.
@@ -29,25 +30,42 @@ class GroupingListBuilder extends EntityListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    if ($entity->access('view', \Drupal::currentUser())) {
-      $row['title'] = $this->l(
-        $entity->getTitle(),
-        new Url(
-          'entity.grouping.canonical', array(
-            'grouping' => $entity->id(),
-          )
-        )
-      );
-      $row['parent'] = empty($entity->get('parent')->entity) ? '' : $this->l(
-        $entity->get('parent')->entity->getTitle(),
-        new Url(
-          'entity.grouping.canonical', array(
-            'grouping' => $entity->get('parent')->entity->id(),
-          )
-        )
-      );
-      return $row + parent::buildRow($entity);
+    $row['title'] = $this->l(
+      $entity->getTitle(),
+      new Url(
+        'entity.grouping.canonical', [
+          'grouping' => $entity->id(),
+        ]
+      )
+    );
+    $row['parent'] = empty($entity->get('parent')->entity) ? '' : $this->l(
+      $entity->get('parent')->entity->getTitle(),
+      new Url(
+        'entity.grouping.canonical', [
+          'grouping' => $entity->get('parent')->entity->id(),
+        ]
+      )
+    );
+    return $row + parent::buildRow($entity);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+    $query = $this->getStorage()->getQuery()
+      ->sort($this->entityType->getKey('id'));
+    // Filter entities for non-admin users.
+    if (\Drupal::currentUser()->id() !== '1') {
+      $grouping_ids = Grouping::getAllGroupingsByUser(\Drupal::currentUser(), FALSE);
+      $query->condition('id', $grouping_ids, 'IN');
     }
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    $result = $query->execute();
+    return $result;
   }
 
 }
